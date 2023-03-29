563c3acdb4dfd609ef067721169d17a302defed0
Crism00 <107272848+Crism00@users.noreply.github.com>
2023-03-28 18:11:23 -0600

    EL CAMBIO SERIO

---
+++ src/app/components/sala/sala.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sala',
+  templateUrl: './sala.component.html',
+  styleUrls: ['./sala.component.css']
+})
+export class SalaComponent {
+}
